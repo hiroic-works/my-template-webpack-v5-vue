8ce93e852d29ff3d5b3a51c033db1f283ea14f4b
h.oda <62193961+hiroic-works@users.noreply.github.com>
2021-12-25 14:51:57 +0900

    プロダクション環境で画像圧縮設定

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,8 @@
 const { merge } = require('webpack-merge');
 const TerserPlugin = require('terser-webpack-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
+const { extendDefaultPlugins } = require("svgo");
 const common = require('./webpack.common.js');
 
 const outputName = '[name]-[chunkhash]';
@@ -28,7 +30,39 @@ module.exports = merge(common({ outputName, assetName, htmlMinifyOption }), {
 				},
 			}),
 			// css
-			new CssMinimizerPlugin()
+			new CssMinimizerPlugin(),
+			new ImageMinimizerPlugin({
+				minimizer: {
+					implementation: ImageMinimizerPlugin.imageminMinify,
+					options: {
+						// Lossless optimization with custom option
+						// Feel free to experiment with options for better result for you
+						plugins: [
+							["gifsicle", { interlaced: true }],
+							["jpegtran", { progressive: true }],
+							["optipng", { optimizationLevel: 5 }],
+							// Svgo configuration here https://github.com/svg/svgo#configuration
+							[
+								"svgo",
+								{
+									plugins: extendDefaultPlugins([
+										{
+											name: "removeViewBox",
+											active: false,
+										},
+										{
+											name: "addAttributesToSVGElement",
+											params: {
+												attributes: [{ xmlns: "http://www.w3.org/2000/svg" }],
+											},
+										},
+									]),
+								},
+							],
+						],
+					},
+				},
+			}),
 		],
 	},
 	target: ['web', 'es5']
